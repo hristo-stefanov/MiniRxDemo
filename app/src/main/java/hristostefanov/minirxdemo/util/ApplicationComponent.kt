6e53e89826898d58b9732176b2b3b83b0c575a92
Hristo M.  Stefanov <hristo.m.stefanov@gmail.com>
2020-05-05 11:48:21 +0300

    refine Application component and module

--- app/src/main/java/hristostefanov/minirxdemo/util/ApplicationComponent.kt
+++ app/src/main/java/hristostefanov/minirxdemo/util/ApplicationComponent.kt
@@ -1,5 +1,7 @@
 package hristostefanov.minirxdemo.util
 
+import android.app.Application
+import dagger.BindsInstance
 import dagger.Component
 import hristostefanov.minirxdemo.presentation.MainViewModel
 import hristostefanov.minirxdemo.ui.ViewModelFactory
@@ -7,6 +9,11 @@ import hristostefanov.minirxdemo.ui.ViewModelFactory
 @ApplicationScope
 @Component(modules = [ApplicationModule::class])
 interface ApplicationComponent {
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance app: Application): ApplicationComponent
+    }
+
     fun getViewModelFactory(): ViewModelFactory
     fun getMainViewModel(): MainViewModel
 }
